db6e96d7f0b068ab3bff4e0250ade29c0c53cc86
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-14 22:47:30 +0300

    Added from rep\programming.

---
+++ HW_1_Task_1/HW_1_Task_1/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿namespace HW_1_Task_1
+{
+    class Sort
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    { 
+                        int swap;
+                        swap = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = swap;
+                    }
+                }
+            }
+        }
+    }
+}
