07cbbc8bd6fc618ed5e089ef0ad35e35983ad6b5
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-15 22:53:17 +0300

    fixed small bugs in Test.cs and in Program.cs.

--- HW_1_Task_2/HW_1_Task_2/Tests.cs
+++ HW_1_Task_2/HW_1_Task_2/Tests.cs
@@ -10,36 +10,30 @@ namespace HW_1_Task_2
             var firstAnswer = "PKKKPP$AAA";
             if (!String.Equals(BWT.Inverse(firstCase), firstAnswer))
             {
-                Console.WriteLine(1);
                 return false;
             }
             if (!String.Equals(BWT.Reverse(firstAnswer) + "$", firstCase))
             {
-                Console.WriteLine(BWT.Reverse(firstAnswer));
                 return false;
             }
             var secondCase = "ANANAS$";
             var secondAnswer = "S$NNAAA";
             if (!String.Equals(BWT.Inverse(secondCase), secondAnswer))
             {
-                Console.WriteLine(3);
                 return false;
             }
             if (!String.Equals(BWT.Reverse(secondAnswer) + "$", secondCase))
             {
-                Console.WriteLine(4);
                 return false;
             }
             var thirdCase = "banana$";
             var thirdAnswer = "annb$aa";
             if (!String.Equals(BWT.Inverse(thirdCase), thirdAnswer))
             {
-                Console.WriteLine(5);
                 return false;
             }
             if (!String.Equals(BWT.Reverse(thirdAnswer) + "$", thirdCase))
             {
-                Console.WriteLine(6);
                 return false;
             }
             return true;
