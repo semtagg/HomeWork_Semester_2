784ad106a66ba550eaa15aad1aa4d6c5a1f09013
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-20 18:37:32 +0300

    add files.

---
+++ HW_2_Task_2/HW_2_Task_2/Calculators.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_2_Task_2
+{
+    interface ICalculator
+    {
+        double GetResult(string[] inputLine);
+    }
+    class FirstCalculator : ICalculator
+    {
+
+        public double GetResult(string[] inputLine)
+        {
+            var array = new int[inputLine.Length];
+
+            int top = -1;
+
+            int x;
+            int res = 0;
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                if (int.TryParse(inputLine[i], out x))
+                {
+                    top++;
+                    array[top] = x;
+                }
+                else
+                {
+                    switch (inputLine[i])
+                    {
+                        case "+":
+                            res = array[top - 1] + array[top];
+                            break;
+                        case "-":
+                            res = array[top - 1] - array[top];
+                            break;
+                        case "*":
+                            res = array[top - 1] * array[top];
+                            break;
+                        case "/":
+                            res = array[top - 1] / array[top];
+                            break;
+                    }
+                    top--;
+                    array[top] = res;
+                }
+            }
+
+            return array[top];
+        }
+    }
+    /*class SecondCalculator : ICalculator
+    {
+        public SecondCalculator(string line)
+        {
+            var inputLine = line;
+        }
+        public string GetLine => throw new NotImplementedException();
+
+        public double GetResult()
+        {
+            throw new NotImplementedException();
+        }
+    }*/
+}
---
+++ HW_2_Task_2/HW_2_Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW_2_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку в постфиксной записи: ");
+            var element = new FirstCalculator();
+            Console.WriteLine(element.GetResult(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries))); ;
+        }
+    }
+}
