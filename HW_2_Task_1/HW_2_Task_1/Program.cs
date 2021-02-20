1023fccf79f5512ff255b3e3f8e73906414f8f0e
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-20 18:42:29 +0300

    add files.

---
+++ HW_2_Task_1/HW_2_Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_2_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
