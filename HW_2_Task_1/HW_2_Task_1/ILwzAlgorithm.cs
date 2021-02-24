629603517d2cbac6daef5ba0508e7899a5976449
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-24 18:22:39 +0300

    added files.

---
+++ HW_2_Task_1/HW_2_Task_1/ILwzAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_2_Task_1
+{
+    interface ILwzAlgorithm
+    {
+        string Compress();
+        string Decompress();
+    }
+}
