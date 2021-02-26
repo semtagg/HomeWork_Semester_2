3996be6d46eab0956fa79896fc063873ff91062a
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-02-26 19:40:43 +0300

    renamed files.

---
+++ HW_2_Task_1/HW_2_Task_1/ILzwAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_2_Task_1
+{
+    interface ILzwAlgorithm
+    {
+        /*bool Compress(string pInputFileName, string pOutputFileName);
+        bool Decompress(string pInputFileName, string pOutputFileName);*/
+
+        void Compress(string inputLine);
+        string Decompress(string outputLine);
+    }
+}
