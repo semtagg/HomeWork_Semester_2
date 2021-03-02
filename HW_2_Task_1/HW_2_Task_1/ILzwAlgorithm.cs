f41f7fd68cabd2f4ef81f6f733b8fe00f497b6fc
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-03-02 21:59:44 +0300

    added lzw compression.

--- HW_2_Task_1/HW_2_Task_1/ILzwAlgorithm.cs
+++ HW_2_Task_1/HW_2_Task_1/ILzwAlgorithm.cs
@@ -6,10 +6,8 @@ namespace HW_2_Task_1
 {
     interface ILzwAlgorithm
     {
-        /*bool Compress(string pInputFileName, string pOutputFileName);
-        bool Decompress(string pInputFileName, string pOutputFileName);*/
-
         void Compress(string inputLine);
-        string Decompress(string outputLine);
+
+        void Decompress(string outputLine);
     }
 }
--- HW_2_Task_1/HW_2_Task_1/Program.cs
+++ HW_2_Task_1/HW_2_Task_1/Program.cs
@@ -5,56 +5,11 @@ namespace HW_2_Task_1
     {
         static void Main(string[] args)
         {
-            /*var result = new LzwAlgorithm();
-            result.Decompress("a");
-            var trie = new Trie();
-            trie.Add("привет");
-            trie.Add("мир");
-            trie.Add("приз");
-            trie.Add("мирный");
-            trie.Add("подарок");
-            trie.Add("проект");
-            trie.Add("прапорщик");
-            trie.Add("правый");
-            trie.Add("год");
-            trie.Add("герой");
-            trie.Add("голубь");
-            trie.Add("прокрастинация");
-            trie.Add("красота");*/
-/*
-            trie.Remove("правый");
-            trie.Remove("мир");
-
-            byte value;
-
-            if (trie.Search("привет", out value))
-            {
-                Console.WriteLine(value);
-            }
-            else
-            {
-                Console.WriteLine("Not find!");
-            }
-            if (trie.Search("мир", out value))
-            {
-                Console.WriteLine(value);
-            }
-            else
-            {
-                Console.WriteLine("Not find!");
-            }
-            if (trie.Search("облако", out value))
-            {
-                Console.WriteLine(value);
-            }
-            else
-            {
-                Console.WriteLine("Not find!");
-            }*/
-
             var s = new LzwAlgorithm();
-            s.Compress("text.txt");
+            s.Compress("123.txt");
             Console.WriteLine("");
+
+            /*s.Decompress("123.txt.zipped");*/
         }
     }
 }
