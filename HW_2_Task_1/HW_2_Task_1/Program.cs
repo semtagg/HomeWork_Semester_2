d11d0eab4bc8d180022f6623ea78c7db1116cf0c
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-03-10 00:58:42 +0300

    minor corrections in Program.cs.

--- HW_2_Task_1/HW_2_Task_1/Program.cs
+++ HW_2_Task_1/HW_2_Task_1/Program.cs
@@ -7,7 +7,7 @@ namespace HW_2_Task_1
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Введите полное имя файла: ");
+            Console.WriteLine("Введите полное имя файла: ");
             Console.WriteLine("Введите ключ:\n1) '-c', если хотите сжать файл.\n2) '-u', если хотите разжать файл.");
             if (args[0] == "-c")
             {
@@ -20,25 +20,6 @@ namespace HW_2_Task_1
             else if (args[0] == "-u")
             {
                 LzwAlgorithm.Decompress(args[1]);
-                Console.WriteLine("Файл был удачно разжат!");
-            }*/
-
-
-            Console.WriteLine("Введите полное имя файла: ");
-            var name = Console.ReadLine();
-            Console.WriteLine("Введите ключ:\n1) '-c', если хотите сжать файл.\n2) '-u', если хотите разжать файл.");
-            var key = Console.ReadLine();
-            if (key == "-c")
-            {
-                LzwAlgorithm.Compress(name);
-                var compressedFileSize = new FileInfo(name);
-                var decompressedFileSize = new FileInfo(name + ".zipped");
-                Console.WriteLine("Файл был удачно сжат!");
-                Console.WriteLine($"Коэффициент сжатия: x {(double)compressedFileSize.Length / decompressedFileSize.Length}");
-            }
-            else
-            {
-                LzwAlgorithm.Decompress(name);
                 Console.WriteLine("Файл был удачно разжат!");
             }
         }
