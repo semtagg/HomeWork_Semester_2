eb04ce87bc2f5253dc1bc9ac49eb16e0d64c1a0e
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-04-16 12:21:47 +0300

    added test2.

---
+++ Test_2/Test_2.Tests/PriorityQueueTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test_2.Tests
+{
+    [TestFixture]
+    class PriorityQueueTest
+    {
+        private PriorityQueue<string> queue;
+
+        [SetUp]
+        public void SetUp()
+        {
+            queue = new PriorityQueue<string>();
+            queue.Enqueue("a", 40);
+            queue.Enqueue("b", 40);
+            queue.Enqueue("c", 40);
+            queue.Enqueue("d", 80);
+            queue.Enqueue("e", 20);
+        }
+
+        [TestCase]
+        public void EnqueueTest()
+        {
+            Assert.IsFalse(queue.IsEmpty());
+        }
+
+        [TestCase]
+        public void DequeueTest()
+        {
+            var result = queue.Dequeue();
+            Assert.AreEqual(result, "d");
+            result = queue.Dequeue();
+            Assert.AreEqual(result, "a");
+            result = queue.Dequeue();
+            Assert.AreEqual(result, "b");
+            result = queue.Dequeue();
+            Assert.AreEqual(result, "c");
+            result = queue.Dequeue();
+            Assert.AreEqual(result, "e");
+            Assert.Throws<NullReferenceException>(() => queue.Dequeue());
+        }
+    }
+}
---
+++ Test_2/Test_2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new PriorityQueue<string>();
+            q.Enqueue("a", 40);
+            q.Enqueue("b", 40);
+            q.Enqueue("c", 40);
+            q.Enqueue("d", 80);
+            q.Enqueue("e", 20);
+            var result = q.Dequeue();
+            result = q.Dequeue();
+            result = q.Dequeue();
+            result = q.Dequeue();
+            result = q.Dequeue();
+        }
+    }
+}
