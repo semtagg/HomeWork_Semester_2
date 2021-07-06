e959b8b2caf8a1eb8cf91d8718b4fce5e5be67af
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-07-06 20:54:14 +0300

    fixed mistakes.

--- HW_6_Task_1/HW_6_Task_1.Tests/MethodsTest.cs
+++ HW_6_Task_1/HW_6_Task_1.Tests/MethodsTest.cs
@@ -1,7 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HW_6_Task_1.Tests
 {
@@ -9,21 +8,33 @@ namespace HW_6_Task_1.Tests
     class MethodsTest
     {
         [TestCase]
-        public void Map()
+        public void MapTest()
         {
+            var firstList = Methods.Map(new List<int>() { 1, 2, 3 }, x => x + 2);
+            Assert.AreEqual(firstList, new List<int>() { 3, 4, 5 });
 
+            var secondList = Methods.Map(new List<int>() { 1, 2, 3 }, x => x.ToString());
+            Assert.AreEqual(secondList, new List<string>() { "1", "2", "3" });
         }
 
         [TestCase]
-        public void Filter()
+        public void FilterTest()
         {
+            var firstList = Methods.Filter(new List<int>() { 1, 2, 3 }, x => x > 1);
+            Assert.AreEqual(firstList, new List<int>() { 2, 3 });
 
+            var secondList = Methods.Filter(new List<string>() { "a", "ab", "abc" }, x => x.Length > 1);
+            Assert.AreEqual(secondList, new List<string>() { "ab", "abc" });
         }
 
         [TestCase]
-        public void Fold()
+        public void FoldTest()
         {
+            var firstValue = Methods.Fold(new List<int>() { 1, 2, 3 }, 1, (x, y) => x * y);
+            Assert.AreEqual(firstValue, 6);
 
+            var secondValue = Methods.Fold(new List<string>() { "a", "ab", "abc" }, "", (x, y) => x + y);
+            Assert.AreEqual(secondValue, "aababc");
         }
     }
 }
--- HW_6_Task_1/HW_6_Task_1/Methods.cs
+++ HW_6_Task_1/HW_6_Task_1/Methods.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HW_6_Task_1
 {
-    static class Methods<T>
+    public class Methods
     {
-        public static List<T> Map(List<T> list, Func<T, T> func)
+        /// <summary>
+        /// Applies the passed function to each item in the passed list.
+        /// </summary>
+        public static List<TOut> Map<TIn, TOut>(List<TIn> list, Func<TIn, TOut> func)
         {
-            var newList = new List<T>();
+            var newList = new List<TOut>();
             foreach (var element in list)
             {
                 newList.Add(func(element));
@@ -16,7 +18,10 @@ namespace HW_6_Task_1
             return newList;
         }
 
-        public static List<T> Filter(List<T> list, Func<T, bool> func)
+        /// <summary>
+        /// Leaves the elements of the passed list for which the passed function returned true.
+        /// </summary>
+        public static List<T> Filter<T>(List<T> list, Func<T, bool> func)
         {
             var newList = new List<T>();
             foreach (var element in list)
@@ -29,7 +34,10 @@ namespace HW_6_Task_1
             return newList;
         }
 
-        public static T Fold(List<T> list, T value, Func<T, T, T> func)
+        /// <summary>
+        /// Accumulates the value of all elements in the list.
+        /// </summary>
+        public static TOut Fold<TIn, TOut>(List<TIn> list, TOut value, Func<TOut, TIn, TOut> func)
         {
             foreach (var element in list)
             {
