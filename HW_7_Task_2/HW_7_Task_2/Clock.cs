ea47929013033d63b75765c8cea4d8866d50a00d
Semen-Tagiltsev <s-tagiltsev2016@yandex.ru>
2021-07-09 11:12:58 +0300

    fixed mistakes.

--- HW_7_Task_2/HW_7_Task_2/Clock.cs
+++ HW_7_Task_2/HW_7_Task_2/Clock.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace HW_7_Task_2
@@ -12,12 +13,8 @@ namespace HW_7_Task_2
         }
 
         private Graphics graphics;
-        private Timer timer = new Timer();
         private int centerX;
         private int centerY;
-        private int secondsHand;
-        private int minutessHand;
-        private int hoursHand;
         private int currentHeight;
         private int currentWidth;
 
@@ -26,18 +23,23 @@ namespace HW_7_Task_2
             this.Invalidate();
         }
 
+        private Matrix RotateAroundCenter(float angle, Point center)
+        {
+            var result = new Matrix();
+            result.RotateAt(angle, center);
+            return result;
+        }
+
         private void Clock_Paint(object sender, PaintEventArgs e)
         {
-            graphics = this.CreateGraphics();
+            graphics = e.Graphics;
             graphics.Clear(Color.White);
 
             var ss = DateTime.Now.Second;
             var mm = DateTime.Now.Minute;
             var hh = DateTime.Now.Hour;
 
-            var handCoord = new int[2];
-
-            currentWidth = Width - 20;
+            currentWidth = Width - 20 ;
             currentHeight = Height - 50;
 
             graphics.DrawEllipse(new Pen(Color.Black, 1f), 3, 3, Math.Min(currentWidth, currentHeight) - 3, Math.Min(currentWidth, currentHeight) - 3);
@@ -49,58 +51,17 @@ namespace HW_7_Task_2
             centerX = Math.Min(currentWidth, currentHeight) / 2;
             centerY = Math.Min(currentWidth, currentHeight) / 2;
 
-            secondsHand = Math.Min(currentWidth, currentHeight) / 2 - 30;
-            minutessHand = Math.Min(currentWidth, currentHeight) / 2 - 50;
-            hoursHand = Math.Min(currentWidth, currentHeight) / 2 - 70;
+            graphics.Transform = RotateAroundCenter(6.0f * ss, new Point(centerX, centerY + 4));
+            graphics.DrawLine(new Pen(Color.Red, 1f), new Point(centerX, centerY + 4), new Point(centerX, 15));
 
-            handCoord = SecondsAndMinutesCoord(ss, secondsHand);
-            graphics.DrawLine(new Pen(Color.Red, 1f), new Point(centerX, centerY + 4), new Point(handCoord[0], handCoord[1]));
+            graphics.Transform = RotateAroundCenter(6.0f * mm, new Point(centerX, centerY + 4));
+            graphics.DrawLine(new Pen(Color.Black, 2f), new Point(centerX, centerY + 4), new Point(centerX, 30));
 
-            handCoord = SecondsAndMinutesCoord(mm, minutessHand);
-            graphics.DrawLine(new Pen(Color.Black, 2f), new Point(centerX, centerY + 4), new Point(handCoord[0], handCoord[1]));
-
-            handCoord = HoursCoord(hh % 12, mm, hoursHand);
-            graphics.DrawLine(new Pen(Color.Gray, 3f), new Point(centerX, centerY + 4), new Point(handCoord[0], handCoord[1]));
+            graphics.Transform = RotateAroundCenter(30.0f * hh, new Point(centerX, centerY + 4));
+            graphics.DrawLine(new Pen(Color.Gray, 3f), new Point(centerX, centerY + 4), new Point(centerX, 50));
 
             this.Text = $"Current time  {hh}:{mm}:{ss}";
             graphics.Dispose();
-        }
-
-        private int[] SecondsAndMinutesCoord(int value, int handLength)
-        {
-            int[] coordinates = new int[2];
-            value *= 6;
-
-            if (value >= 0 && value <= 180)
-            {
-                coordinates[0] = centerX + (int)(handLength * Math.Sin(Math.PI * value / 180));
-                coordinates[1] = centerY - (int)(handLength * Math.Cos(Math.PI * value / 180));
-            }
-            else
-            {
-                coordinates[0] = centerX - (int)(handLength * -Math.Sin(Math.PI * value / 180));
-                coordinates[1] = centerY - (int)(handLength * Math.Cos(Math.PI * value / 180));
-            }
-            return coordinates;
-        }
-
-        private int[] HoursCoord(int hoursValue, int minutes, int handLength)
-        {
-            int[] coordinates = new int[2];
-
-            int val = (int)((hoursValue * 30) + (minutes * 0.5));
-
-            if (val >= 0 && val <= 180)
-            {
-                coordinates[0] = centerX + (int)(handLength * Math.Sin(Math.PI * val / 180));
-                coordinates[1] = centerY - (int)(handLength * Math.Cos(Math.PI * val / 180));
-            }
-            else
-            {
-                coordinates[0] = centerX - (int)(handLength * -Math.Sin(Math.PI * val / 180));
-                coordinates[1] = centerY - (int)(handLength * Math.Cos(Math.PI * val / 180));
-            }
-            return coordinates;
         }
     }
 }
