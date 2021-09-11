24cc0fe144cd97ea87c0468e299181890cfa151e
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-09-12 01:03:32 +0300

    wip

--- Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
+++ Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -2,11 +2,14 @@
 
 namespace ParallelMatrixMultiplication
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var m1 = FileManager.ReadFromFile("matrix1.txt");
+            var m2 = FileManager.ReadFromFile("matrix2.txt");
+            var r = MatrixManager.MatrixMultiplication(m1, m2);
+            FileManager.WriteToFile(r, "r.txt");
         }
     }
 }
