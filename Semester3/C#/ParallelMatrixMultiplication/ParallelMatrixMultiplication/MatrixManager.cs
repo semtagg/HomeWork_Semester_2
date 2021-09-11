10287084c06530d6e91eda7bc9b49773687d4457
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-09-12 01:03:32 +0300

    wip

--- Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixManager.cs
+++ Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixManager.cs
@@ -1,7 +1,34 @@
-﻿namespace ParallelMatrixMultiplication
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace ParallelMatrixMultiplication
 {
-    public class MatrixManager
+    public static class MatrixManager
     {
-        
+        public static int[,] MatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            // size
+            var result = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+            var threads = new List<Thread>();
+            for (var i = 0; i < firstMatrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < secondMatrix.GetLength(1); j++)
+                {
+                    var columnIndex = i;
+                    var rowIndex = j;
+                    var thread = new Thread(() =>
+                    {
+                        for (var k = 0; k < firstMatrix.GetLength(0); k++)
+                        {
+                            result[columnIndex, rowIndex] += firstMatrix[columnIndex, k] * secondMatrix[k, rowIndex];
+                        }});
+                    thread.Start();
+                    threads.Add(thread);
+                }
+            }
+            foreach (var t in threads)
+                t.Join();
+            return result;
+        }
     }
 }
