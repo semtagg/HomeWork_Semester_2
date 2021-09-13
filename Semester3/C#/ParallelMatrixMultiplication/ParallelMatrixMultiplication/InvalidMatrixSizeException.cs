9a3c68ed75e9cd4ee2664f6ff9eb9ec43fcc18a1
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-09-13 17:35:26 +0300

    added tests.

---
+++ Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/InvalidMatrixSizeException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ParallelMatrixMultiplication
+{
+    /// <summary>
+    /// An exception is thrown if the dimensions of the matrix are not comparable.
+    /// </summary>
+    [Serializable]
+    public class InvalidMatrixSizeException : Exception
+    {
+        public InvalidMatrixSizeException()
+        {
+        }
+
+        public InvalidMatrixSizeException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidMatrixSizeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected InvalidMatrixSizeException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
