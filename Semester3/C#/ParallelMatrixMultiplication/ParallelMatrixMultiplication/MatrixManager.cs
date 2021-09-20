7fd49625a3d24883d5090e64e04b09ef4891d3db
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-09-20 13:21:18 +0300

    wip

--- Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixManager.cs
+++ Semester3/C#/ParallelMatrixMultiplication/ParallelMatrixMultiplication/MatrixManager.cs
@@ -88,5 +88,36 @@ namespace ParallelMatrixMultiplication
 
             return result;
         }
+        
+        public static int[,] RandomMatrix(int rowNumber, int columnNumber)
+        {
+            var matrix = new int[rowNumber, columnNumber];
+            for (var i = 0; i < rowNumber; i++)
+            {
+                for (var j = 0; j < columnNumber; j++)
+                {
+                    matrix[i, j] = new Random().Next() % 100;
+                }    
+            }
+
+            return matrix;
+        }
+
+        public static int[,] OrdinaryMatrixMultiplication(int[,] m1, int[,] m2)
+        {
+            var result = new int[m1.GetLength(0), m2.GetLength(1)];
+            for (var i = 0; i < m1.GetLength(0); i++)
+            {
+                for (var j = 0; j < m2.GetLength(1); j++)
+                {
+                    for (var k = 0; k < m2.GetLength(0); k++)
+                    {
+                        result[i,j] += m1[i,k] * m2[k,j];
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
