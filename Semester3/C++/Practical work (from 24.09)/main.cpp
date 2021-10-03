df569698d03ce5f5e493c574d869897e13650369
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-03 15:30:22 +0300

    add task5

--- Semester3/C++/Practical work (from 24.09)/main.cpp
+++ Semester3/C++/Practical work (from 24.09)/main.cpp
@@ -34,22 +34,40 @@ bool task4_1(int x) {
     return ~x & 1;
 }
 
-void task4_2(int& x, int& y){
+void task4_2(int &x, int &y) {
     x = x ^ y;
     y = x ^ y;
     x = x ^ y;
 }
 
-int task4_3(int x)
-{
+int task4_3(int x) {
     return x << 3;
+}
+
+void task5(int t, float v, float &s1, float &s2, float &s) {
+    s1 = t * v;
+    s2 = t * v / 2;
+    s = s1 + s2;
 }
 
 int main() {
     task1(1);
-    cout << task2(16, 2) << endl;
-    cout << task3(20, 2) << endl;
 
+    cout << "Task 2: " << task2(16, 2) << endl;
+
+    cout << "Task 3: " << task3(20, 2) << endl;
+
+    cout << "Task 4.1: " << task4_1(1) << endl;
+
+    int x = 1, y = 2;
+    task4_2(x, y);
+    cout << "Task 4.2: x = " << x << " ,y = " << y << endl;
+
+    cout << "Task 4.3: " << task4_3(5) << endl;
+
+    float a, b, c;
+    task5(10, 5, a, b, c);
+    cout << a << " " << b << " " << c << endl;
 
     return 0;
 }
