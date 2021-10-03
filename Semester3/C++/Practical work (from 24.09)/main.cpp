fa7fe2c7bfb59fa1d12c81f7b0fdcb1d768760ee
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-03 15:15:33 +0300

    add task4

--- Semester3/C++/Practical work (from 24.09)/main.cpp
+++ Semester3/C++/Practical work (from 24.09)/main.cpp
@@ -30,9 +30,26 @@ int task3(int x, int i) {
     return x & ~(1 << i);
 }
 
+bool task4_1(int x) {
+    return ~x & 1;
+}
+
+void task4_2(int& x, int& y){
+    x = x ^ y;
+    y = x ^ y;
+    x = x ^ y;
+}
+
+int task4_3(int x)
+{
+    return x << 3;
+}
+
 int main() {
     task1(1);
     cout << task2(16, 2) << endl;
     cout << task3(20, 2) << endl;
+
+
     return 0;
 }
