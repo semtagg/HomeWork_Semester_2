cdb0341a0234f1cbad2b910a8a12679c3da3c9b7
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-06 14:09:40 +0300

    add m_a

--- Semester3/C++/Practical work (from 01.10)/main.cpp
+++ Semester3/C++/Practical work (from 01.10)/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "cmath"
+#include <algorithm>
 
 using namespace std;
 
@@ -11,10 +12,42 @@ int e_b(int r, float x, float y) {
     return r * r - (x * x + y * y) >= -10e-6 ? 1 : 0;
 }
 
-int e_d (int y, int m, int d) {
+int e_d(int y, int m, int d) {
     if (m < 3)
         y--, m += 12;
-    return 365*y + y/4 - y/100 + y/400 + (153*m - 457)/5 + d - 306;
+    return 365 * y + y / 4 - y / 100 + y / 400 + (153 * m - 457) / 5 + d - 306;
+}
+
+unsigned int gcd(unsigned int n1, unsigned int n2) {
+    return (n2 == 0) ? n1 : gcd(n2, n1 % n2);
+}
+
+int m_a(int v1, int v2, int v3) {
+    if (v3 > v1 + v2)
+        return false;
+    else if (v1 == v3 || v2 == v3 || v1 + v2 == v3)
+        return true;
+    else
+        return v3 % gcd(v1, v2) == 0;
+}
+
+bool is_prime(int x)
+{
+    int p = 2;
+    while (p * p <= x)
+    {
+        if (x % p == 0)
+            return false;
+        p++;
+    }
+    return true;
+}
+
+bool m_2(int a, int b)
+{
+    if (a/2 < b)
+        return false;
+
 }
 
 int h_a_simple(int A, int D) {
@@ -28,6 +61,8 @@ int h_a_simple(int A, int D) {
 int main() {
     cout << e_a(10, 12) << endl;
     cout << e_b(1, 0.8, 0.6) << endl;
-    cout << e_d(2001,3,1) - e_d(2001,2,1);
+    cout << e_d(2001, 3, 1) - e_d(2001, 2, 1) << endl;
+    cout << m_a(3,5,4)<< endl;
+
     return 0;
 }
