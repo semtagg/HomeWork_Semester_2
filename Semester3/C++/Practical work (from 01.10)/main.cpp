e1139aa5ea3d0918b43780042e88f0b5a28752fa
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-06 11:49:26 +0300

    add e_a, e_b, e_d

--- Semester3/C++/Practical work (from 01.10)/main.cpp
+++ Semester3/C++/Practical work (from 01.10)/main.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+float e_a(int a, int S) {
+    return (float) 2 * S / a;
+}
+
+int e_b(int r, float x, float y) {
+    return r * r - (x * x + y * y) >= -10e-6 ? 1 : 0;
+}
+
+int e_d (int y, int m, int d) {
+    if (m < 3)
+        y--, m += 12;
+    return 365*y + y/4 - y/100 + y/400 + (153*m - 457)/5 + d - 306;
+}
+
 int h_a_simple(int A, int D) {
     int One = 1;
     for (int i = 1; i < A; ++i) {
@@ -12,9 +26,8 @@ int h_a_simple(int A, int D) {
 }
 
 int main() {
-    cout << h_a_simple(1, 2) << endl;
-    cout << h_a_simple(2, 3) << endl;
-    cout << h_a_simple(3, 4) << endl;
-    cout << h_a_simple(4, 5) << endl;
+    cout << e_a(10, 12) << endl;
+    cout << e_b(1, 0.8, 0.6) << endl;
+    cout << e_d(2001,3,1) - e_d(2001,2,1);
     return 0;
 }
