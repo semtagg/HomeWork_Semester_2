6e463218a068c9763701fe541e08d0f46f53ca0c
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-29 13:17:42 +0300

    added task1

--- Semester3/C++/Practical work (from 29.10)/task3.cpp
+++ Semester3/C++/Practical work (from 29.10)/task3.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int singleNumber() {
-    int n;
-    int result = 0;
-
-    while (cin >> n) {
-        result ^= n;
-    }
-
-    return result;
-}
-
-int main() {
-    cout << singleNumber();
-    return 0;
-}
